f3f3c2ed9f0340e23a30010853f159f52af348b2
31884 <2296395079xia@gmail.com>
2019-03-20 16:26:00 +0800

    circularly operate

--- simulateApp.py
+++ simulateApp.py
@@ -10,14 +10,13 @@ headers = {
 
 
 def get_password(str):
-    
     js = json.loads(str)
     content = js['topic']['content']
     for each in content:
         link = re.search('https://[\w./-]+', each['infor'])
-        if each['infor'].find('才能浏览')>0:
+        if each['infor'].find('才能浏览') > 0:
             return '需要付费'
-        if each['infor'].find('查看提取码请回复')>0:
+        if each['infor'].find('查看提取码请回复') > 0:
             return '查看提取码请回复'
         if link:
             print(link.group())
@@ -30,12 +29,14 @@ def get_password(str):
 # url='www.fumankong.com'
 url = 'https://www.fumankong.com//mobcent/app/web/index.php?r=forum/postlist'
 date = 'packageName=com.appbyme.app301097&forumType=7&pageSize=10&accessToken=e8576268548d8c3ed48ea959225d5&appName=%E8%85%90%E6%BC%AB%E6%8E%A7&topicId={id}&authorId=0&egnVersion=v2103.5&accessSecret=29d3743008928d453836f122b008a&sdkVersion=2.5.0.0&imei=99001064700010&apphash=ddf6b0a4&boardId=88&forumKey=q0PvFAdGj0lwMvKxw6&page=1&platType=1&imsi=460110210578654&sdkType='
-# id=146951
-print('请输入id')
-id = input()
+while True:
+    # id=146951
+    print('请输入id')
+    id = input()
 
-response = requests.post(url, date.format(id=id), headers=headers)
-# print(response.content.decode('gb2312'))
-json_text = response.content.decode(response.apparent_encoding)
-print(get_password(json_text))
-input('运行完成，按任意键退出')
+    response = requests.post(url, date.format(id=id), headers=headers)
+    # print(response.content.decode('gb2312'))
+    json_text = response.content.decode(response.apparent_encoding)
+    print(get_password(json_text))
+    if input('空格键继续，其他键退出') != ' ':
+        break
