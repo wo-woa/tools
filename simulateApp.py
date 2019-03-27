c9cf1a33b92dc792ca3f83b8d2ca4bae947e26ee
31884 <318844856@qq.com>
2019-03-27 09:21:11 +0800

    beautify json output

--- simulateApp.py
+++ simulateApp.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import re
 import requests
@@ -7,7 +8,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
-
 
 def get_password(str):
     js = json.loads(str)
@@ -23,14 +23,15 @@ def get_password(str):
         result = re.search('验证码：\w+|提取码.\w+|密码[:：]\w+|\[sell=\d+?,\d+?\][A-Za-z0-9_]+', each['infor'])
         if result:
             return result.group()
-    return str
+    # 美化输出json中带有中文的
+    return json.dumps(content, sort_keys=True, indent=4,separators=(',', ':'),ensure_ascii=False)
 
 
 # url='www.fumankong.com'
 url = 'https://www.fumankong.com//mobcent/app/web/index.php?r=forum/postlist'
 date = 'packageName=com.appbyme.app301097&forumType=7&pageSize=10&accessToken=e8576268548d8c3ed48ea959225d5&appName=%E8%85%90%E6%BC%AB%E6%8E%A7&topicId={id}&authorId=0&egnVersion=v2103.5&accessSecret=29d3743008928d453836f122b008a&sdkVersion=2.5.0.0&imei=99001064700010&apphash=ddf6b0a4&boardId=88&forumKey=q0PvFAdGj0lwMvKxw6&page=1&platType=1&imsi=460110210578654&sdkType='
 while True:
-    # id=146951
+    # id=155176
     print('请输入id')
     id = input()
 
