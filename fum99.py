dacf10c94fccdd6a6adc72757327794349f19ed2
31884 <318844856@qq.com>
2019-03-31 16:35:07 +0800

    add another column 'type' and a new type url to download

--- fum99.py
+++ fum99.py
@@ -14,10 +14,11 @@ class fum99():
             data = requests.get(url, headers=self.headers)
             html = etree.HTML(data.text)
             header_list=html.xpath('//tbody/tr/th/a[2]')
+            type_list=html.xpath('//tbody/tr/th/em/a')
             content_list=[]
-            for each in header_list:
-                href=each.xpath('./@href')[0]
-                content_list.append(each.text+',http://fum99.com/'+str(href))
+            for i in range(len(header_list)):
+                href=header_list[i].xpath('./@href')[0]
+                content_list.append(type_list[i].text+','+header_list[i].text+',http://fum99.com/'+str(href))
             return content_list
         except Exception as err:
             print(err)
@@ -33,10 +34,21 @@ class fum99():
             print('run')
             print(err)
 
+    def run2(self, page):
+        try:
+            with open(self.name + '.csv', 'a', encoding='utf-8') as file:
+                for i in range(page):
+                    content = self.get_content(self.url + str(i + 1) + '.html')
+                    for each in content:
+                        file.write(each + '\n')
+                    print('成功添加' + str(len(content)) + '行')
+        except Exception as err:
+            print('run')
+            print(err)
 
-
-url='http://fum99.com/forum.php?mod=forumdisplay&fid=46&filter=typeid&typeid=79'
+# url='https://www.danmei99.com/forum-44-'
+url='https://www.danmei99.com/forum.php?mod=forumdisplay&fid=44&filter=typeid&typeid=69'
 name='sr'
 a=fum99(url,name)
-a.run(62)
+a.run(29)
 
