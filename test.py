83ca7de65b5b5c613adb913edfaaffba38f7a356
simon <318844856@qq.com>
2019-06-11 23:17:11 +0800

    test py2 can't open file with chinese name

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import json
+import sys
+import os
 
-dic = '{"rs":1,"errcode":""}'
-print(type(json.loads(dic)))
-js = json.dumps(json.loads(dic), sort_keys=True, indent=4)
-print(js)
+path=r'E:\tools\sqliteman-win32\Sqliteman-1.2.2\imageformats'
+path='F:\手机\书\杂'
+for root,dirs,files in os.walk(path):
+    for i in files:
+        print(i)
