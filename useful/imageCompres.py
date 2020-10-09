dd5a5867549ab1304b6f45e573ef015357595f61
simon <318844856@qq.com>
2020-10-09 18:37:31 +0800

    循环操作 多线程

--- useful/imageCompres.py
+++ useful/imageCompres.py
@@ -10,6 +10,22 @@ desc: https://www.cnblogs.com/li1992/p/10675769.html
 from PIL import Image
 import os
 import shutil
+import re
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+def my_input(tip, pattern=None, default=None):
+    while True:
+        text = input(tip)
+        data = None
+        if text == '' and default != None:
+            return default
+        if pattern:
+            data = re.match(pattern, text)
+        if data:
+            return text
+        else:
+            print("请输入正确格式！")
 
 
 def get_size(file):
@@ -19,11 +35,11 @@ def get_size(file):
 
 
 def get_outfile(infile, outfile):
-    ''' 如果格式为png，压缩效率会比较低,默认jpg
+    """ 如果格式为png，压缩效率会比较低,默认jpg
     :param infile:
     :param outfile:
     :return:
-    '''
+    """
     if outfile:
         return outfile
     dir, suffix = os.path.splitext(infile)
@@ -46,7 +62,7 @@ def resize_image(infile, outfile='', x_s=1376):
     out.save(outfile)
 
 
-def compress_image(infile, outfile='', mb=150, step=10, quality=50):
+def compress_image(infile, outfile='', mb=150, step=10, quality=50, name=''):
     """不改变图片尺寸压缩到指定大小
     :param infile: 压缩源文件
     :param outfile: 压缩文件保存地址
@@ -64,28 +80,32 @@ def compress_image(infile, outfile='', mb=150, step=10, quality=50):
         im = Image.open(infile)
         if quality - step < 0:
             break
-        im=im.convert('RGB')
+        im = im.convert('RGB')
         im.save(outfile, quality=quality)
         quality -= step
         o_size = get_size(outfile)
+    print(name+"压缩完成。")
     return outfile, get_size(outfile)
 
 
 def compress_folder():
-    type = input("请输入图片压缩类型(1.文件夹（默认）。2.单文件)：")
-    type = '1' if type == '' else type
-    mb = input("请输入压缩目标(KB,默认150)：")
-    mb = 150 if mb == '' else int(mb)
-    step = input("请输入每次调整的压缩比率(默认10)：")
-    step = 10 if step == '' else int(step)
-    quality = input("请输入初始压缩比率(默认50)：")
-    quality = 50 if quality == '' else int(quality)
-    address = input("请输入压缩路径：")
+    address = my_input("请输入压缩路径：", r'^\w:\\.+$')
+    type = my_input("请输入图片压缩类型(1.文件夹（默认）。2.单文件)：", '^\d$', '1')
+    mb = int(my_input("请输入压缩目标(KB,默认500)：", '^\d+$', '500'))
+    step = int(my_input("请输入每次调整的压缩比率(默认5)：", '^\d+$', '5'))
+    quality = int(my_input("请输入初始压缩比率(默认50)：", '^\d+$', '50'))
     if type == '1':
         files = os.listdir(address)
-        for each in files:
-            print(each + '压缩中...')
-            compress_image(os.path.join(address, each), mb=mb, step=step, quality=quality)
+        with ThreadPoolExecutor(max_workers=5) as t:
+            obj_list = []
+            for each in files:
+                obj = t.submit(compress_image, os.path.join(address, each), mb=mb, step=step, quality=quality,
+                               name=each)
+            for future in as_completed(obj_list):
+                future.result()
+        # for each in files:
+        #     print(each + '压缩中...')
+        #     compress_image(os.path.join(address, each), mb=mb, step=step, quality=quality)
     else:
         compress_image(address, mb=mb, step=step, quality=quality)
 
@@ -93,5 +113,7 @@ def compress_folder():
 if __name__ == '__main__':
     # i, a = compress_image(r'E:\[Lenxus][Hombretigre]\fmk10-out.jpg', mb=750, step=3, quality=4)
     # compress_image(r'E:\wf\漫画家\37.2℃\封面\[37.2℃] いつかさらばさ-狗爹汉化组.png',mb=150, step=3, quality=4)
-    compress_folder()
-    input("压缩完成！")
+    while True:
+        compress_folder()
+        if input('压缩完成！回车退出，非空继续:') == '':
+            break
